68cf616716c97d8b587cbd8d8a2eeffa1722985a
Will Sullivan <wsulliv8@users.noreply.github.com>
2026-06-26 15:34:30 -0700

    Texture

--- project/main.cpp
+++ project/main.cpp
@@ -1,5 +1,6 @@
 #include <glad/glad.h>
 #define GLFW_INCLUDE_NONE
+#include "stb_image.h"
 #include <GLFW/glfw3.h>
 
 #include "shader.hpp"
@@ -70,6 +71,33 @@ int main() {
   }
 
   glViewport(0, 0, 800, 600);
+
+  // Texture Start
+
+  unsigned int texture;
+  glGenTextures(1, &texture);
+  glBindTexture(GL_TEXTURE_2D, texture);
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  int width, height, nrChannels;
+  unsigned char *data =
+      stbi_load("container.jpg", &width, &height, &nrChannels, 0);
+
+  if (data) {
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
+                 GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+  } else {
+    std::cout << "Failed to load texture" << std::endl;
+  }
+
+  stbi_image_free(data);
+
+  // Texture End
 
   unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
   glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
